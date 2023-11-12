345f3b43a87dc0c171975d56d91db6d251cfe72a
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-11-13 00:43:31 +0100

    cors issue solved

--- images-management-server/app.js
+++ images-management-server/app.js
@@ -8,35 +8,17 @@ require('./db');
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require('./config')(app);
 
-// Configurar CORS
-app.use(
-  cors({
-    allowedHeaders: ['authorization', 'Content-Type'], // you can change the headers
-    exposedHeaders: ['authorization'], // you can change the headers
-    origin: 'http://192.168.1.163:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false
-  })
-);
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-// 👇 Start handling routes here
 const indexRoutes = require('./routes/index.routes');
 app.use('/api', indexRoutes);
 
 const imageRoutes = require('./routes/image.routes');
 app.use('/api/images', imageRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
 module.exports = app;
--- images-management-server/db/index.js
+++ images-management-server/db/index.js
@@ -1,12 +1,13 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/images-management-server";
+  process.env.MONGODB_URI ||
+  'mongodb://127.0.0.1:27017/images-management-server';
 
 mongoose
   .connect(MONGO_URI)
@@ -15,5 +16,5 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+    console.error('Error connecting to mongo: ', err);
   });
